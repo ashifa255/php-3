aee09c1527511442bc1c2be766db9e42bc8452d2
ashifa255 <astammannahifa0330@gmail.com>
2025-05-19 22:06:13 +0600

    Load data info for viewing the data list

--- iendex.php
+++ iendex.php
@@ -18,3 +18,27 @@ function loadData()
     $data = file_get_contents(PROJECT_FILES);
     return $data ? json_decode($data, true) : [];
 }
+
+$info = loadData();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['task']) && !empty(trim($_POST['task']))) {
+        $info[] = [
+            'data' => htmlspecialchars($_POST['task']),
+            'done' => false
+        ];
+        saveData($info);
+        header('Location:' . $_SERVER['PHP_SELF']);
+        exit;
+    } elseif (isset($_POST['delete'])) {
+        unset($info[$_POST['delete']]);
+        saveData($info);
+        header('Location:' . $_SERVER['PHP_SELF']);
+        exit;
+    } elseif (isset($_POST['toggle'])) {
+        $info[$_POST['toggle']]['done'] = !$info[$_POST['toggle']]['done'];
+        saveData($info);
+        header('Location:' . $_SERVER['PHP_SELF']);
+        exit;
+    }
+}
